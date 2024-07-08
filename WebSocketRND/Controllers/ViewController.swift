7adade1ba5a9d316dbce357cf477e3ce541da480
Muhammad Asad <m.asad.chatthaa@gmnail.com>
2024-07-08 12:46:50 +0500

    create Message View

--- WebSocketRND/Controllers/ViewController.swift
+++ WebSocketRND/Controllers/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     let webSocketClient = WebSocketClient(url: URL(string: "ws://localhost:8080")!)
+    var messages: [(String, Bool)] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,3 +22,22 @@ class ViewController: UIViewController {
     }
 }
 
+// MARK: - UITableViewDelegate
+
+extension ViewController: UITableViewDelegate {
+    
+}
+
+// MARK: - UITableViewDataSource
+
+extension ViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return messages.count
+        return messages.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
